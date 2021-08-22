3c9839baf19fb82371db826bd77efb876aa98e8f
wy <wy@world.com>
2021-08-22 16:26:30 +0800

    1.0-SNAPSHOT

--- src/main/java/com/wybusy/EasyLucene.java
+++ src/main/java/com/wybusy/EasyLucene.java
@@ -8,19 +8,17 @@ import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.search.highlight.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class EasyLucene {
     private String path;
@@ -28,6 +26,10 @@ public class EasyLucene {
     private Directory directory;
     private SmartChineseAnalyzer analyzer;
     private IndexWriterConfig iwConfig;
+
+    public String punctuation = "[\\[\\]\\{\\}\\s\\,\\.\\?!? ?—\":@\\*'\\-\\(\\)\\\\/;%（）。，：“”；！？、《》【】｛｝～＠＃￥％…＆×÷－＋＝\u3000‘’·]+";
+    public String stripTag = "(<.*?>)";
+    public Double luceneMaxMatchValue = 0.96;
 
     public EasyLucene(String path, String name) throws IOException {
         this.path = path;
@@ -38,19 +40,16 @@ public class EasyLucene {
         iwConfig = new IndexWriterConfig(analyzer);
     }
 
-    public Integer append(List<EasyLuceneData> data) {
-        // 设置创建索引模式(无，创建后增加，有则在原来的索引的基础上新增)
-        iwConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
-        return write(data);
+    public Integer write(List<EasyLuceneData> data) {
+        return write(data, true); //默认是追加
     }
 
-    public Integer creat(List<EasyLuceneData> data) {
-        //创建索引，删除之前的索引
-        iwConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-        return write(data);
-    }
-
-    private Integer write(List<EasyLuceneData> data) {
+    public Integer write(List<EasyLuceneData> data, boolean append) {
+        if (append) { // 设置创建索引模式(无，创建后增加，有则在原来的索引的基础上新增)
+            iwConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+        } else {//创建索引，删除之前的索引
+            iwConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        }
         Integer result = 0;
         IndexWriter indexWriter = null;
         try {
@@ -72,14 +71,33 @@ public class EasyLucene {
             // 关闭事务
             indexWriter.close();
         } catch (IOException e) {
-            if (indexWriter != null) {
-                try {
-                    indexWriter.close();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-            }
             e.printStackTrace();
+        } finally {
+            try {
+                indexWriter.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    public boolean del(String id) {
+        boolean result = true;
+        IndexWriter indexWriter = null;
+        try {
+            indexWriter = new IndexWriter(directory, iwConfig);
+            indexWriter.deleteDocuments(new Term("id", id));
+            indexWriter.commit();
+        } catch (IOException e) {
+            result = false;
+            e.printStackTrace();
+        } finally {
+            try {
+                indexWriter.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
         }
         return result;
     }
@@ -134,4 +152,77 @@ public class EasyLucene {
         String str = highlight.getBestFragment(analyzer, fieldName, fieldContent);
         return str;
     }
+
+    public double similarity(String inputText, String storeText) {
+        double result = 0;
+
+        inputText = textToWord(inputText);
+        storeText = textToWord(storeText);
+
+        if (inputText != null && inputText.trim().length() > 0 && storeText != null
+                && storeText.trim().length() > 0) {
+
+            Map<String, int[]> AlgorithmMap = new HashMap<>();
+
+            String[] input = inputText.toLowerCase().split(punctuation);
+            String[] store = storeText.toLowerCase().split(punctuation);
+
+            for (int i = 0; i < input.length; i++) {
+                String d1 = input[i];
+                int[] fq = AlgorithmMap.get(d1);
+                if (fq != null && fq.length == 2) {
+                    fq[0]++;
+                } else {
+                    fq = new int[2];
+                    fq[0] = 1;
+                    fq[1] = 0;
+                    AlgorithmMap.put(d1, fq);
+                }
+            }
+
+            for (int i = 0; i < store.length; i++) {
+                String d2 = store[i];
+                int[] fq = AlgorithmMap.get(d2);
+                if (fq != null && fq.length == 2) {
+                    fq[1]++;
+                } else {
+                    fq = new int[2];
+                    fq[0] = 0;
+                    fq[1] = 1;
+                    AlgorithmMap.put(d2, fq);
+                }
+            }
+
+            Iterator<String> iterator = AlgorithmMap.keySet().iterator();
+            double sqdoc1 = 0;
+            double sqdoc2 = 0;
+            double denominator = 0;
+            while (iterator.hasNext()) {
+                int[] c = AlgorithmMap.get(iterator.next());
+                denominator += c[0] * c[1];
+                sqdoc1 += c[0] * c[0];
+                sqdoc2 += c[1] * c[1];
+            }
+
+            result = denominator / Math.sqrt(sqdoc1 * sqdoc2);
+        }
+        return result;
+    }
+
+    public String textToWord(String text) {
+        QueryParser parser = new QueryParser("", analyzer);
+        String result = "";
+        try {
+            BooleanQuery.setMaxClauseCount(10000);
+            Query query = parser.parse(text);
+            result = query.toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        result = result.replaceAll("[\\(\\)]", "") //替换（）
+                .replaceAll("\\d+", "") //替换数字
+                .replaceAll(" \\w ", " "); //替换单个字母
+        return result;
+    }
+
 }
